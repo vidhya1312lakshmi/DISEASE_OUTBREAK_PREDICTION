66ab27910cacc185d670ae104733a28363510b4e
vidhyalakshmi <vidhya1312lakshmi@gmail.com>
2025-02-23 20:59:41 +0530

    Fixed ValueError in input conversion and improved error handling

--- app.py/web.py
+++ app.py/web.py
@@ -2,124 +2,179 @@ import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-st.set_page_config(page_title='DISEASE OUTBREAK PREDICTION SYSTEM', 
+
+st.set_page_config(page_title='Prediction of Disease Outbreaks', 
                    layout='wide', 
-                   page_icon="💉🧪🧫" )
-diabetes_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\diabetes_model.sav",'rb'))
-heart_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\heart_model.sav",'rb'))
-parkinsons_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\parkinsons_model.sav",'rb'))
+                   page_icon="💉🧪🧫")
 
+# Load trained ML models
+diabetes_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\diabetes_model.sav", 'rb'))
+heart_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\heart_model.sav", 'rb'))
+parkinsons_model = pickle.load(open(r"C:\Users\vidhy\OneDrive\Documents\disease outbreak\training_model\parkinsons_model.sav", 'rb'))
+
+# Sidebar menu
 with st.sidebar:
-    selected = option_menu('Prediction of Disease Outbreak System',['Diabetes Prediction','Heart Disease Prediction','Parkinsons Prediction'],
-                           menu_icon='hospital-fill',icons=['activity','heart','person'],default_index=0)
+    selected = option_menu(
+        'Prediction of Disease Outbreak System',
+        ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'],
+        menu_icon='hospital-fill', icons=['activity', 'heart', 'person'], default_index=0
+    )
 
+# ================= Diabetes Prediction =================
 if selected == 'Diabetes Prediction':
     st.title('Diabetes Prediction Using ML')
-    col1,col2,col3 = st.columns(3)
+    
+    col1, col2, col3 = st.columns(3)
+    
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
+        Pregnancies = st.text_input('Number of Pregnancies', value="0")
     with col2:
-        Glucose = st.text_input('Glucose Level')
+        Glucose = st.text_input('Glucose Level', value="0")
     with col3:
-        Bloodpressure = st.text_input('Blood Pressure Value')
+        Bloodpressure = st.text_input('Blood Pressure Value', value="0")
     with col1:
-        Skinthickness = st.text_input('Skin Thickness Value')
+        Skinthickness = st.text_input('Skin Thickness Value', value="0")
     with col2:
-        Insulin = st.text_input('Insulin Level')
+        Insulin = st.text_input('Insulin Level', value="0")
     with col3:
-        BMI = st.text_input('BMI Value')
+        BMI = st.text_input('BMI Value', value="0")
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value', value="0")
     with col2:
-        Age = st.text_input('Age of the person')
+        Age = st.text_input('Age of the person', value="0")
 
-if selected == 'Heart Disease Prediction':
+    diab_diagnosis = ''
+
+    if st.button('Diabetes Test Result'):
+        try:
+            user_input = [float(Pregnancies), float(Glucose), float(Bloodpressure), float(Skinthickness),
+                          float(Insulin), float(BMI), float(DiabetesPedigreeFunction), float(Age)]
+            
+            diab_prediction = diabetes_model.predict([user_input])
+            
+            if diab_prediction[0] == 1:
+                diab_diagnosis = 'The Person is Diabetic'
+            else:
+                diab_diagnosis = 'The Person is Not Diabetic'
+        
+        except ValueError:
+            diab_diagnosis = "Error: Please enter valid numerical values."
+
+        st.success(diab_diagnosis)
+
+# ================= Heart Disease Prediction =================
+elif selected == 'Heart Disease Prediction':
     st.title('Heart Disease Prediction Using ML')
-    col1,col2,col3 = st.columns(3)
-    with col1:
-        age = st.text_input('Age of the person')
-    with col2:
-        sex = st.text_input('Gender of the person')
-    with col3:
-        cp = st.text_input('CP Value')
-    with col1:
-        trestbps = st.text_input('Trestbps Value')
-    with col2:
-        chol = st.text_input('Chol Value')
-    with col3:
-        fbs = st.text_input('Fbs Value')
-    with col1:
-        restecg = st.text_input('Restecg Value')
-    with col2:
-        thalach = st.text_input('Thalach Value')
-    with col3:
-        exang = st.text_input('Exang Value')
-    with col1:
-        oldpeak = st.text_input('Oldpeak Value')
-    with col2:
-        slope = st.text_input('Slope Value')
-    with col3:
-        ca = st.text_input('Ca Value')
-    with col1:
-        thal = st.text_input('Thal Value')
+    
+    col1, col2, col3 = st.columns(3)
 
-if selected == 'Parkinsons Prediction':
-    st.title('Parkinsons Prediction Using ML')
-    col1,col2,col3 = st.columns(3)
     with col1:
-        name = st.text_input('Name of the person')
+        age = st.text_input('Age of the person', value="0")
     with col2:
-        MDVP_fo = st.text_input('MDVP:Fo(Hz) Value')
+        sex = st.text_input('Gender (1 = Male, 0 = Female)', value="0")
     with col3:
-        MDVP_fhi = st.text_input('MDVP:Fhi(Hz) Value')
+        cp = st.text_input('Chest Pain Type (CP)', value="0")
     with col1:
-        MDVP_flo = st.text_input('MDVP:Flo(Hz) Value')
+        trestbps = st.text_input('Resting Blood Pressure (Trestbps)', value="0")
     with col2:
-        MDVP_jit = st.text_input('MDVP:Jitter(%) Value')
+        chol = st.text_input('Cholesterol Level', value="0")
     with col3:
-        MDVP_jitter = st.text_input('MDVP:Jitter(Abs) Value')
+        fbs = st.text_input('Fasting Blood Sugar (1 = True, 0 = False)', value="0")
     with col1:
-        MDVP_rap = st.text_input('MDVP:RAP Value')
+        restecg = st.text_input('Rest ECG Result', value="0")
     with col2:
-        MDVP_ppq = st.text_input('MDVP:PPQ Value')
+        thalach = st.text_input('Max Heart Rate Achieved (Thalach)', value="0")
     with col3:
-        Jitter_ddp = st.text_input('Jitter:DDP Value')
+        exang = st.text_input('Exercise Induced Angina (1 = Yes, 0 = No)', value="0")
     with col1:
-        MDVP_Shimmer = st.text_input('MDVP:Shimmer Value')
+        oldpeak = st.text_input('ST Depression (Oldpeak)', value="0")
     with col2:
-        MDVP_Shimmerdb = st.text_input('MDVP:Shimmer(dB) Value')
+        slope = st.text_input('Slope of Peak Exercise ST Segment', value="0")
     with col3:
-        Shimmer_APQ3 = st.text_input('Shimmer:APQ3 Value')
+        ca = st.text_input('Number of Major Vessels (Ca)', value="0")
     with col1:
-        Shimmer_APQ5 = st.text_input('Shimmer:APQ5 Value')
-    with col2:
-        MDVP_APQ = st.text_input('MDVP:APQ Value')
-    with col3:
-        Shimmer_DDA = st.text_input('Shimmer:DDA Value')
-    with col1:
-        NHR = st.text_input('NHR Value')
-    with col2:
-        HNR = st.text_input('HNR Value')
-    with col3:
-        RPDE = st.text_input('RPDE Value')
-    with col1:
-        DFA = st.text_input('DFA Value')
-    with col2:
-        spread1 = st.text_input('spread1 Value')
-    with col3:
-        spread2 = st.text_input('spread2 Value')
-    with col1:
-        D2 = st.text_input('D2 Value')
-    with col2:
-        PPE = st.text_input('PPE Value')
+        thal = st.text_input('Thalassemia Type (Thal)', value="0")
 
-diab_diagnosis = ''
-if st.button('Diabetes Test Result'):
-    user_input=[Pregnancies,Glucose,Bloodpressure,Skinthickness,Insulin,BMI,DiabetesPedigreeFunction,Age]
-    user_input=[float(x) for x in user_input]
-    diab_prediction = diabetes_model.predict([user_input])
-    if diab_prediction[0]==1:
-        diab_diagnosis='The Person is Diabetic'
-    else:
-        diab_diagnosis='The Person is Not Diabetic'
-st.success(diab_diagnosis)
+    heart_diagnosis = ''
+
+    if st.button('Heart Disease Test Result'):
+        try:
+            user_input = [float(age), float(sex), float(cp), float(trestbps), float(chol), float(fbs),
+                          float(restecg), float(thalach), float(exang), float(oldpeak), float(slope),
+                          float(ca), float(thal)]
+            
+            heart_prediction = heart_model.predict([user_input])
+            
+            if heart_prediction[0] == 1:
+                heart_diagnosis = 'The Person has Heart Disease'
+            else:
+                heart_diagnosis = 'The Person does not have Heart Disease'
+        
+        except ValueError:
+            heart_diagnosis = "Error: Please enter valid numerical values."
+
+        st.success(heart_diagnosis)
+
+# ================= Parkinson’s Prediction =================
+elif selected == 'Parkinsons Prediction':
+    st.title('Parkinson’s Prediction Using ML')
+
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        MDVP_fo = st.text_input('MDVP:Fo(Hz) Value', value="0")
+    with col2:
+        MDVP_fhi = st.text_input('MDVP:Fhi(Hz) Value', value="0")
+    with col3:
+        MDVP_flo = st.text_input('MDVP:Flo(Hz) Value', value="0")
+    with col1:
+        MDVP_jit = st.text_input('MDVP:Jitter(%) Value', value="0")
+    with col2:
+        MDVP_jitter = st.text_input('MDVP:Jitter(Abs) Value', value="0")
+    with col3:
+        MDVP_rap = st.text_input('MDVP:RAP Value', value="0")
+    with col1:
+        MDVP_ppq = st.text_input('MDVP:PPQ Value', value="0")
+    with col2:
+        Jitter_ddp = st.text_input('Jitter:DDP Value', value="0")
+    with col3:
+        MDVP_Shimmer = st.text_input('MDVP:Shimmer Value', value="0")
+    with col1:
+        MDVP_Shimmerdb = st.text_input('MDVP:Shimmer(dB) Value', value="0")
+    with col2:
+        NHR = st.text_input('NHR Value', value="0")
+    with col3:
+        HNR = st.text_input('HNR Value', value="0")
+    with col1:
+        RPDE = st.text_input('RPDE Value', value="0")
+    with col2:
+        DFA = st.text_input('DFA Value', value="0")
+    with col3:
+        spread1 = st.text_input('Spread1 Value', value="0")
+    with col1:
+        spread2 = st.text_input('Spread2 Value', value="0")
+    with col2:
+        D2 = st.text_input('D2 Value', value="0")
+    with col3:
+        PPE = st.text_input('PPE Value', value="0")
+
+    parkinsons_diagnosis = ''
+
+    if st.button('Parkinson’s Test Result'):
+        try:
+            user_input = [float(MDVP_fo), float(MDVP_fhi), float(MDVP_flo), float(MDVP_jit), float(MDVP_jitter),
+                          float(MDVP_rap), float(MDVP_ppq), float(Jitter_ddp), float(MDVP_Shimmer),
+                          float(MDVP_Shimmerdb), float(NHR), float(HNR), float(RPDE), float(DFA),
+                          float(spread1), float(spread2), float(D2), float(PPE)]
+            
+            parkinsons_prediction = parkinsons_model.predict([user_input])
+            
+            if parkinsons_prediction[0] == 1:
+                parkinsons_diagnosis = 'The Person has Parkinson’s Disease'
+            else:
+                parkinsons_diagnosis = 'The Person does not have Parkinson’s Disease'
+        
+        except ValueError:
+            parkinsons_diagnosis = "Error: Please enter valid numerical values."
+
+        st.success(parkinsons_diagnosis)
